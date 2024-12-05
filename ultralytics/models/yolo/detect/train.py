181415f835a4a1aa04a58a3caa6f8740e5a5f063
GilSeamas <gil.simas@sea.ai>
2024-12-05 16:12:51 +0000

    make override in validator instead of validator arguments (if not val would break if called outide of train)

--- ultralytics/models/yolo/detect/train.py
+++ ultralytics/models/yolo/detect/train.py
@@ -93,10 +93,8 @@ class DetectionTrainer(BaseTrainer):
     def get_validator(self):
         """Returns a DetectionValidator for YOLO model validation."""
         self.loss_names = "box_loss", "cls_loss", "dfl_loss"
-        val_args = copy(self.args)
-        val_args.single_cls = val_args.single_cls_val        
         return yolo.detect.DetectionValidator(
-            self.test_loader, save_dir=self.save_dir, args=val_args, _callbacks=self.callbacks
+            self.test_loader, save_dir=self.save_dir, args=copy(self.args), _callbacks=self.callbacks
         )
 
     def label_loss_items(self, loss_items=None, prefix="train"):
--- ultralytics/models/yolo/detect/val.py
+++ ultralytics/models/yolo/detect/val.py
@@ -37,6 +37,7 @@ class DetectionValidator(BaseValidator):
         self.is_lvis = False
         self.class_map = None
         self.args.task = "detect"
+        self.args.single_cls = self.args.single_cls_val
         self.metrics = DetMetrics(save_dir=self.save_dir, on_plot=self.on_plot, args=self.args)
         mAP_lb = 0.5 if not hasattr(self.args, 'mAP_lb') else self.args.mAP_lb
         mAP_ub = 0.95 if not hasattr(self.args, 'mAP_ub') else self.args.mAP_ub
